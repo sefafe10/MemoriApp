76f1c3f3102e53e0f214e7201458b4d470fe0a2a
Sergio <sfarinasf22@fpcoruna.afundacion.org>
2024-05-21 14:47:54 +0200

    Cambio del boton Corazón por el botón SOS

--- app/src/main/java/com/example/memoriapp/HomeFragment.kt
+++ app/src/main/java/com/example/memoriapp/HomeFragment.kt
@@ -1,19 +1,27 @@
 package com.example.memoriapp
 
+import android.Manifest
 import android.content.ActivityNotFoundException
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.PorterDuff
+import android.net.Uri
 import android.os.Bundle
+import android.provider.ContactsContract
+import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageButton
 import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
-import android.provider.MediaStore
-import android.provider.ContactsContract
-
 
 class HomeFragment : Fragment() {
+
+    private val REQUEST_CALL_PERMISSION = 1
+    private val SOS_PHONE_NUMBER = " "
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -28,12 +36,21 @@ class HomeFragment : Fragment() {
         val libretaDeContactosButton = view.findViewById<ImageButton>(R.id.libreta_de_contactos)
         val camaraButton = view.findViewById<ImageButton>(R.id.camara)
         val calendarioButton = view.findViewById<ImageButton>(R.id.calendario)
-        val corazonButton = view.findViewById<ImageButton>(R.id.corazon)
+        val sosButton = view.findViewById<ImageButton>(R.id.sos)
+
+        // Crear un color negro
+        val colorNegro = ContextCompat.getColor(requireContext(), android.R.color.black)
+
+        // Aplicar el color negro a los drawables de los ImageButtons
+        usuarioButton.setColorFilter(colorNegro, PorterDuff.Mode.SRC_IN)
+        microfonoButton.setColorFilter(colorNegro, PorterDuff.Mode.SRC_IN)
+        libretaDeContactosButton.setColorFilter(colorNegro, PorterDuff.Mode.SRC_IN)
+        camaraButton.setColorFilter(colorNegro, PorterDuff.Mode.SRC_IN)
+        calendarioButton.setColorFilter(colorNegro, PorterDuff.Mode.SRC_IN)
 
         // Configurar OnClickListener para cada botón
         usuarioButton.setOnClickListener {
-            // Acción cuando se hace clic en el botón de usuario
-            // Aquí puedes agregar la lógica correspondiente
+            // Aquí puedes añadir la funcionalidad que deseas para este botón
         }
 
         microfonoButton.setOnClickListener {
@@ -55,27 +72,18 @@ class HomeFragment : Fragment() {
                 // Manejar la excepción si la actividad no está disponible
                 Toast.makeText(context, "No se pudo abrir la aplicación de contactos", Toast.LENGTH_SHORT).show()
             }
-
-
-
-
         }
-
 
         camaraButton.setOnClickListener {
             val intent = Intent(Intent.ACTION_VIEW, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
             startActivity(intent)
         }
 
-
-
-
         calendarioButton.setOnClickListener {
             val intent = Intent(Intent.ACTION_MAIN).apply {
                 // Configurar el paquete de la aplicación de calendario (puede variar según el dispositivo)
                 setClassName("com.google.android.calendar", "com.android.calendar.AllInOneActivity")
             }
-
             try {
                 startActivity(intent)
             } catch (e: ActivityNotFoundException) {
@@ -84,13 +92,32 @@ class HomeFragment : Fragment() {
             }
         }
 
-
-
-        corazonButton.setOnClickListener {
-            // Acción cuando se hace clic en el botón de corazón
-            // Aquí puedes agregar la lógica correspondiente
+        sosButton.setOnClickListener {
+            makePhoneCall()
         }
 
         return view
+    }
+
+
+    private fun makePhoneCall() {
+        if (ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(requireActivity(), arrayOf(Manifest.permission.CALL_PHONE), REQUEST_CALL_PERMISSION)
+        } else {
+            val callIntent = Intent(Intent.ACTION_CALL)
+            callIntent.data = Uri.parse("tel:$SOS_PHONE_NUMBER")
+            startActivity(callIntent)
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == REQUEST_CALL_PERMISSION) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                makePhoneCall()
+            } else {
+                Toast.makeText(requireContext(), "Permiso para realizar llamadas denegado", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
