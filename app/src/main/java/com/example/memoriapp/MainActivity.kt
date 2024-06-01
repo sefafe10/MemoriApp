5853d5ab87eba142c6e5d6705b408ba0169bc4c6
Sergio <sfarinasf22@fpcoruna.afundacion.org>
2024-06-01 20:01:44 +0200

    Eliminadas las referencias a fragmento innecesario

--- app/src/main/java/com/example/memoriapp/MainActivity.kt
+++ app/src/main/java/com/example/memoriapp/MainActivity.kt
@@ -26,7 +26,7 @@ class MainActivity : AppCompatActivity() {
         // menu should be considered as top level destinations.
         val appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.navigation_home, R.id.navigation_surprise, R.id.navigation_profile
+                R.id.navigation_home, R.id.navigation_profile
             )
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
