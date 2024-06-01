42ad263f7d06e848dac31a9f5bb85ae9f8f45338
Sergio <sfarinasf22@fpcoruna.afundacion.org>
2024-06-01 23:08:31 +0200

    Corregidos algunos errores de compatibilidad

--- app/src/main/java/com/example/memoriapp/LauncherActivity.kt
+++ app/src/main/java/com/example/memoriapp/LauncherActivity.kt
@@ -10,14 +10,14 @@ class LauncherActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.loading_screen)
         // Recuperamos el nombre de usuario de las preferencias
-        val preferences = getSharedPreferences("MySharedPref", MODE_PRIVATE)
-        val username = preferences.getString("USERNAME", null) // null será el valor por defecto
+        val preferences = getSharedPreferences("UserInfo", MODE_PRIVATE)
+        val name = preferences.getString("name", null) // null será el valor por defecto
         Handler().postDelayed({
             // Si el usuario nunca ha iniciado la app, el valor es 'null' por defecto
             // Se inicia la pantalla de información del usuario
-            if (username == null) {
-                val loginActivity = Intent(this@LauncherActivity, UserInfoActivity::class.java)
-                startActivity(loginActivity)
+            if (name == null) {
+                val userActivity = Intent(this@LauncherActivity, UserInfoActivity::class.java)
+                startActivity(userActivity)
             } else {
                 // Si el usuario SÍ se ha logueado, ya disponemos de su nombre de usuario
                 // Se inicia la pantalla principal
